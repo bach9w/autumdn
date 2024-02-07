c040a6a9ed6c3b8488fd7ad93a032783d7c971d2
bach9w <78446671+bach9w@users.noreply.github.com>
2024-02-07 18:11:25 +0200

    Update Navbar logo image

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import Logo from "/public/logo2.png";
+import Logo from "/public/opt1.png";
 
 import CustomButton from "./CustomButton";
 
