7fd87ec8b05371e92134d47de79a09a40d6c14d8
bach9w <78446671+bach9w@users.noreply.github.com>
2024-02-08 16:03:14 +0200

    Update product titles and thumbnails

--- components/Parallax.tsx
+++ components/Parallax.tsx
@@ -11,51 +11,51 @@ export function HeroParallaxDemo() {
 }
 export const products = [
 	{
-		title: "Ferarri",
+		title: "GMC",
 		link: "http://ferrari.com",
 		thumbnail:
-			"https://cdn.ferrari.com/cms/network/media/img/resize/65365327f0abd70024e699ee-ferrari729151piufondo_1920x1080?",
+			"https://cs.copart.com/v1/AUTH_svc.pdoc00001/lpp/0124/988e3d756b654d9da5448c431290f749_ful.jpg",
+	},
+	{
+		title: "2016 AUDI Q5 PREMIUM PLUS",
+		link: "http://ferrari.com",
+		thumbnail:
+			"https://cs.copart.com/v1/AUTH_svc.pdoc00001/lpp/1023/0d8eaf4360674fffab988cebde1eb810_ful.jpg",
+	},
+	{
+		title: "2019 TOYOTA HIGHLANDER SE",
+		link: "http://ferrari.com",
+		thumbnail:
+			"https://cs.copart.com/v1/AUTH_svc.pdoc00001/lpp/0124/1b7fdc5588104650b5f793145777e909_ful.jpg",
+	},
+	{
+		title: "2023 MERCEDES-BENZ S 500 4MATIC",
+		link: "http://ferrari.com",
+		thumbnail:
+			"https://cs.copart.com/v1/AUTH_svc.pdoc00001/lpp/0124/63259c0b3eac429d8f0aabbdf32c65e9_ful.jpg",
+	},
+	{
+		title: "202 MERCEDES-BENZ S 500 4MATIC",
+		link: "http://ferrari.com",
+		thumbnail:
+			"https://cs.copart.com/v1/AUTH_svc.pdoc00001/lpp/0124/63259c0b3eac429d8f0aabbdf32c65e9_ful.jpg",
+	},
+	{
+		title: "2023 MERCEDES-BENZ S 500 4MATIC",
+		link: "http://ferrari.com",
+		thumbnail:
+			"https://cs.copart.com/v1/AUTH_svc.pdoc00001/lpp/0124/63259c0b3eac429d8f0aabbdf32c65e9_ful.jpg",
+	},
+	{
+		title: "2022 MERCEDES-BENZ GLE COUPE AMG 53 4MATIC",
+		link: "http://ferrari.com",
+		thumbnail:
+			"https://cs.copart.com/v1/AUTH_svc.pdoc00001/lpp/1223/3a868578ed434d21afe70faf2744364a_ful.jpg",
 	},
 	{
 		title: "Ferarri",
 		link: "http://ferrari.com",
 		thumbnail:
-			"https://cdn.ferrari.com/cms/network/media/img/resize/5d26fdb7c3f9ec0af6475619-01_fb_ppl_intro_lp3lhwq8?width=1080",
-	},
-	{
-		title: "Ferarri",
-		link: "http://ferrari.com",
-		thumbnail:
-			"https://cdn.ferrari.com/cms/network/media/img/resize/65365327f0abd70024e699ee-ferrari729151piufondo_1920x1080?",
-	},
-	{
-		title: "Ferarri",
-		link: "http://ferrari.com",
-		thumbnail:
-			"https://cdn.ferrari.com/cms/network/media/img/resize/5d26fdb7c3f9ec0af6475619-01_fb_ppl_intro_lp3lhwq8?width=1080",
-	},
-	{
-		title: "Ferarri",
-		link: "http://ferrari.com",
-		thumbnail:
-			"https://cdn.ferrari.com/cms/network/media/img/resize/65365327f0abd70024e699ee-ferrari729151piufondo_1920x1080?",
-	},
-	{
-		title: "Ferarri",
-		link: "http://ferrari.com",
-		thumbnail:
-			"https://cdn.ferrari.com/cms/network/media/img/resize/5d26fdb7c3f9ec0af6475619-01_fb_ppl_intro_lp3lhwq8?width=1080",
-	},
-	{
-		title: "Ferarri",
-		link: "http://ferrari.com",
-		thumbnail:
-			"https://cdn.ferrari.com/cms/network/media/img/resize/65365327f0abd70024e699ee-ferrari729151piufondo_1920x1080?",
-	},
-	{
-		title: "Ferarri",
-		link: "http://ferrari.com",
-		thumbnail:
-			"https://cdn.ferrari.com/cms/network/media/img/resize/5d26fdb7c3f9ec0af6475619-01_fb_ppl_intro_lp3lhwq8?width=1080",
+			"https://cs.copart.com/v1/AUTH_svc.pdoc00001/lpp/1223/5752181e5e5e4cbc8506757ddd211878_ful.jpg",
 	},
 ];
--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ["cdn.imagin.studio", "wallpapercave.com", "cdn.ferrari.com"],
+		domains: [
+			"cdn.imagin.studio",
+			"wallpapercave.com",
+			"cdn.ferrari.com",
+			"cs.copart.com",
+		],
 	},
 };
 
