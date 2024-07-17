7cb9c34c5c3ec453e4d9aa6bce2ab6b9f4363623
bach9w <78446671+bach9w@users.noreply.github.com>
2024-07-17 19:12:32 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,14 @@ import { Montserrat } from "next/font/google";
 import StickyBottomNav from "@/components/bottom/sticky-bottom";
 import { Analytics } from "@vercel/analytics/react";
 import Cookie from "@components/bottom/cookie/Cookie";
+import Head from "next/head";
 
 export const metadata = {
   title: "MYRIDE",
   description: "MYRIDE IMPORT",
+  openGraph: {
+    images: "/myride_logo.jpg",
+  },
 };
 
 export const viewport: Viewport = {
@@ -28,6 +32,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta property="og:image" content="/myride_logo.jpg" />
+        <meta property="og:title" content="MyRide.BG" />
+        <meta
+          property="og:description"
+          content="Внос на автомобили от Америка и Канада"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://myride.bg" />
+      </Head>
       <body className="relative">
         <main className={cn(montserrat.className, "bg-orange-700")}>
           <ConvexClientProvider>
