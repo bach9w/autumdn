838afbc07f372bbeb7baf37bf3eb60fea9f362f9
bach9w <78446671+bach9w@users.noreply.github.com>
2024-07-25 22:16:48 +0300

    update

--- app/auction/components/Pagination.tsx
+++ app/auction/components/Pagination.tsx
@@ -3,6 +3,7 @@ import { Button } from "@components/ui/button";
 import { usePathname, useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
 import { useCallback } from "react";
+
 const Pagination = ({ page }: { page: any }) => {
   const router = useRouter();
   const pathname = usePathname();
@@ -12,52 +13,37 @@ const Pagination = ({ page }: { page: any }) => {
     (name: string, value: string) => {
       const params = new URLSearchParams(searchParams.toString());
       params.set(name, value);
-
       return params.toString();
     },
     [searchParams],
   );
+
   return (
     <div className="flex w-full items-center justify-center gap-2">
-      {page !== "1" ||
-        ("" && (
-          <Button
-            onClick={() => {
-              router.push(
-                pathname +
-                  "?" +
-                  createQueryString("page", String(Number(page) - 1)),
-              );
-            }}
-          >
-            Предишна страница
-          </Button>
-        ))}
-      {page === "" ? (
+      {page !== "1" && (
         <Button
           onClick={() => {
             router.push(
               pathname +
                 "?" +
-                createQueryString("page", String(Number(page) + 2)),
+                createQueryString("page", String(Number(page) - 1)),
             );
           }}
         >
-          Следваща страница
-        </Button>
-      ) : (
-        <Button
-          onClick={() => {
-            router.push(
-              pathname +
-                "?" +
-                createQueryString("page", String(Number(page) + 1)),
-            );
-          }}
-        >
-          Следваща страница
+          Предишна страница
         </Button>
       )}
+      <Button
+        onClick={() => {
+          router.push(
+            pathname +
+              "?" +
+              createQueryString("page", String(Number(page) + 1)),
+          );
+        }}
+      >
+        Следваща страница
+      </Button>
     </div>
   );
 };
