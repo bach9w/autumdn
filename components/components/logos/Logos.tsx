a3376fffc7b76ab0df4f50e2d07cbc4fa57696e4
bach9w <78446671+bach9w@users.noreply.github.com>
2024-09-29 15:30:49 +0300

    update

--- components/components/logos/Logos.tsx
+++ components/components/logos/Logos.tsx
@@ -3,26 +3,7 @@ import { motion } from "framer-motion";
 import React from "react";
 import { IconType } from "react-icons";
 import {
-  SiNike,
-  Si3M,
-  SiAbstract,
-  SiAdobe,
-  SiAirtable,
-  SiAmazon,
-  SiBox,
-  SiBytedance,
-  SiChase,
-  SiCloudbees,
-  SiBurton,
   SiBmw,
-  SiHeroku,
-  SiBuildkite,
-  SiCouchbase,
-  SiDailymotion,
-  SiDeliveroo,
-  SiEpicgames,
-  SiGenius,
-  SiGodaddy,
   SiAudi,
   SiMercedes,
   SiAstonmartin,
@@ -43,28 +24,6 @@ import {
   SiMazda,
   SiTesla,
 } from "react-icons/si";
-const Logo = () => {
-  // Temp logo from https://logoipsum.com/
-  return (
-    <svg
-      width="32"
-      height="auto"
-      viewBox="0 0 50 39"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-fit fill-zinc-950"
-    >
-      <path
-        d="M16.4992 2H37.5808L22.0816 24.9729H1L16.4992 2Z"
-        stopColor="#09090B"
-      ></path>
-      <path
-        d="M17.4224 27.102L11.4192 36H33.5008L49 13.0271H32.7024L23.2064 27.102H17.4224Z"
-        stopColor="#09090B"
-      ></path>
-    </svg>
-  );
-};
 
 export const Logos = () => {
   return (
